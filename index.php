af422af52749d7e3d8686f0f076f268591d62ed2
sid <72088895+sid-spector@users.noreply.github.com>
2023-12-11 17:33:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ require __DIR__ . '/vendor/autoload.php';
 use Cowsayphp\Farm;
 
 header('Content-Type: text/plain');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
+#header('Access-Control-Allow-Origin: *');
+#header('Access-Control-Allow-Methods: GET, POST');
 
-header("Content-Security-Policy: default-src 'self'");
+#header("Content-Security-Policy: default-src 'self'");
 
-header("Strict-Transport-Security 'max-age=16070400'");
-header("X-XSS-Protection: 0");
-header("X-Frame-Options: DENY");
+#header("Strict-Transport-Security 'max-age=16070400'");
+#header("X-XSS-Protection: 0");
+#header("X-Frame-Options: DENY");
 
 $text = "Set a message by adding ?message=<message here> to the URL";
 if(isset($_GET['message']) && $_GET['message'] != '') {
